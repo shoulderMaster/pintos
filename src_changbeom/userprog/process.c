5fde0fd09dc8fedae1a906c4797fa5e518dc8f9d
ChangBeom <ckdqja0225@gmail.com>
2018-05-22 17:19:14 -0700

    userprog/process.c::load_segment() : ELF 로드 시 page를 직접 할당하는 것이 아닌 vm_entry를 이용하여 재구현

--- src_changbeom/userprog/process.c
+++ src_changbeom/userprog/process.c
@@ -720,12 +720,10 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
-      /* Get a page of memory. */
-      uint8_t *kpage = palloc_get_page (PAL_USER);
+      /* uint8_t *kpage = palloc_get_page (PAL_USER);
       if (kpage == NULL)
         return false;
 
-      /* Load this page. */
       if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
         {
           palloc_free_page (kpage);
@@ -733,12 +731,31 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
         }
       memset (kpage + page_read_bytes, 0, page_zero_bytes);
 
-      /* Add the page to the process's address space. */
       if (!install_page (upage, kpage, writable)) 
         {
           palloc_free_page (kpage);
           return false; 
-        }
+        } */
+           
+      /*  vm_entry 생성 (malloc 사용) */
+      struct vm_entry *vme = (struct vm_entry*)malloc (sizeof (struct vm_entry));
+      memset (vme, NULL, sizeof (struct vm_entry));
+      if (vme == NULL) 
+        return false;
+      
+      /*  vm_entry 멤버들 설정, 가상페이지가 요구될 때 읽어야할 파일의 오프
+       셋과 사이즈, 마지막에 패딩할 제로 바이트 등등 */
+      vme->type = VM_FILE;
+      vme->writable = writable;
+      vme->is_loaded = false;
+      vme->file = file;
+      vme->offset = ofs;
+      vme->read_bytes = read_bytes;
+      vme->zero_bytes = zero_bytes;
+      
+
+      /*  insert_vme() 함수를 사용해서 생성한 vm_entry를 해시테이블에 추가 */
+      insert_vme (thread_current ()->vm, vme);
 
       /* Advance. */
       read_bytes -= page_read_bytes;
