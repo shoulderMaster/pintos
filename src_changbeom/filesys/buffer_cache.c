1dbdc97f28261adc6b7e09fc5fca40190c1c733c
ChangBeom <ckdqja0225@gmail.com>
2018-06-10 21:45:28 -0700

    bc_read() 함수 추가

--- src_changbeom/filesys/buffer_cache.c
+++ src_changbeom/filesys/buffer_cache.c
@@ -29,7 +29,7 @@ void bc_init (void) {
   memset (buffer_head_table, 0x00, sizeof (struct buffer_head));
   for (i = 0; i < BUFFER_CACHE_ENTRY_NB; i++) {
     lock_init (&buffer_head_table[i].bc_lock);
-    buffer_head_table[i].bc_entry = p_buffer_cache + i*BLOCK_SIZE;
+    buffer_head_table[i].bc_entry = p_buffer_cache + i*BLOCK_SECTOR_SIZE;
   }
 }
 
@@ -105,22 +105,48 @@ struct buffer_head* bc_select_victim (void) {
 bool bc_write (block_sector_t sector_idx, void *buffer,
     off_t bytes_written, int chunk_size, int sector_ofs) {
   bool success = false;
-  struct buffer_head *bce = NULL;
+  struct buffer_head *bch = NULL;
   /*  sector_idx를 buffer_head에서 검색하여 buffer에 복사(구현)*/
-  bce = bc_lookup (sector_idx);
-  if (bce == NULL) {
-    bce = bc_select_victim ();
-    lock_acquire (&bce->bc_lock);
-    block_read (fs_device, sector_idx, bce->bc_entry);
-    lock_release (&bce->bc_lock);
+  bch = bc_lookup (sector_idx);
+  if (bch == NULL) {
+    bch = bc_select_victim ();
+    lock_acquire (&bch->bc_lock);
+    block_read (fs_device, sector_idx, bch->bc_entry);
+    lock_release (&bch->bc_lock);
   }
 
-  lock_acquire (&bce->bc_lock);
-  memcpy (bce->bc_entry + sector_ofs, buffer, bytes_written);
-  bce->dirty = true;
-  bce->clock_bit = false;
-  lock_release (&bce->bc_lock);
+  lock_acquire (&bch->bc_lock);
+  memcpy (bch->bc_entry + sector_ofs, buffer, bytes_written);
+  bch->dirty = true;
+  bch->clock_bit = false;
+  lock_release (&bch->bc_lock);
   success = true;
   /*  update buffer head (구현) */
   return success;
+}
+
+bool bc_read (block_sector_t sector_idx, void *buffer,
+              off_t bytes_read, int chunk_size, int sector_ofs) {
+  /*  sector_idx를 buffer_head에서 검색 (bc_lookup 함수 이용) */
+  struct buffer_head *bch = NULL;
+  bch = bc_lookup (sector_idx);
+  /*  검색 결과가 없을 경우, 디스크 블록을 캐싱 할 buffer entry의
+      buffer_head를 구함 (bc_select_victim 함수 이용) */
+  if (bch == NULL) {
+    bch = bc_select_victim ();
+    ASSERT (bch->in_use == false);
+    bch->sector = sector_idx;
+  }
+  /*  block_read 함수를 이용해, 디스크 블록 데이터를 buffer cache로 read */
+  lock_acquire (&bch->bc_lock);
+  block_read (fs_device, sector_idx, bch->bc_entry);
+  /*  memcpy 함수를 통해, buffer에 디스크 블록 데이터를 복사 */
+  memcpy (buffer, bch->bc_entry + sector_ofs, chunk_size);
+  /*  buffer_head의 clock bit을 setting */
+  bch->in_use = true;
+  bch->clock_bit = false;
+  bch->dirty = false;
+
+  lock_release (&bch->bc_lock);
+  return true;
 }
