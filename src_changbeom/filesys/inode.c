e5161590c1ade1152a8f96b0020152c4546c0cbb
ChangBeom <ckdqja0225@gmail.com>
2018-06-20 08:00:55 -0700

    free_inode_sectors() 함수 구현

--- src_changbeom/filesys/inode.c
+++ src_changbeom/filesys/inode.c
@@ -279,6 +279,61 @@ bool inode_update_file_length (struct inode_disk* inode_disk, off_t start_pos, o
   return true;
 }
 
+static void free_inode_sectors (struct inode_disk *inode_disk){
+  struct inode_indirect_block *ind_block_1 = NULL;
+  struct inode_indirect_block *ind_block_2 = NULL;
+  int i = 0, j = 0;
+
+  ind_block_1 = (struct indirect_block_sec*)malloc (BLOCK_SECTOR_SIZE);
+  ind_block_2 = (struct indirect_block_sec*)malloc (BLOCK_SECTOR_SIZE);
+
+  /*  Double indirect 방식으로 할당된 블록 해지 */
+  if (inode_disk->double_indirect_block_sec > 0) {
+    /*  1차 인덱스 블록을 buffer cache에서 읽음 */
+    bc_read (inode_disk->double_indirect_block_sec, ind_block_1, 0, BLOCK_SECTOR_SIZE, 0);
+    i = 0;
+    /*  1차 인덱스 블록을 통해 2차 인덱스 블록을 차례로 접근 */
+    while (ind_block_1->map_table[i] > 0) {
+      /*  2차 인덱스 블록을 buffer cache에서 읽음 */
+      j = 0;
+      bc_read (ind_block_2->map_table[j], ind_block_2, 0, BLOCK_SECTOR_SIZE, 0);
+      /*  2차 인덱스 블록에 저장된 디스크 블록 번호를 접근 */
+      while (ind_block_2->map_table[j] > 0) {
+        /*  free_map 업데이틀 통해 디스크 블록 할당 해지 */
+        free_map_release (ind_block_2->map_table[j], 1);
+        j++;
+      }
+      /*  2차 인덱스 블록 할당 해지 */
+      free_map_release (ind_block_1->map_table[i], 1);
+      i++;
+    }
+  }
+
+  /*  1차 인덱스 블록 할당 해지 */
+  /*  Indirect 방식으로 할당된 디스크 블록 해지*/
+  if(inode_disk->indirect_block_sec > 0){
+    /*  인덱스 블록을 buffer cache에서 읽음 */
+    bc_read (inode_disk->indirect_block_sec, ind_block_1, 0, BLOCK_SECTOR_SIZE, 0);
+    i = 0;
+    /*  인덱스 블록에 저장된 디스크 블록 번호를 접근 */
+    while(ind_block_1->map_table[i] > 0){
+      /*  free_map 업데이트를 통해 디스크 블록 할당 해지 */
+      free_map_release (ind_block_1->map_table[i], 1);
+      i++;
+    }
+    /*  Direct 방식으로 할당된 디스크 블록 해지*/
+    while (inode_disk->direct_map_table[i] > 0){
+      /*  free_map 업데이트를 통해 디스크 블록 할당 해지 */
+      free_map_release (inode_disk->direct_map_table[i], 1);
+      i++;
+    }
+  }
+
+  free (ind_block_1);
+  free (ind_block_2);
+
+}
+
 /* List of open inodes, so that opening a single inode twice
    returns the same `struct inode'. */
 static struct list open_inodes;
